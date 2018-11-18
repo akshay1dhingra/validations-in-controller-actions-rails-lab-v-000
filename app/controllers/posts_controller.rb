92e83c321a07c3939d6e4ff2bcc6c8b102eb85d3
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-11-18 21:10:02 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,8 @@ class PostsController < ApplicationController
   def update
     if @post.update(post_params)
       redirect_to post_path(@post)
-    else 
-      render :edit 
+    else
+      render :edit
     end
   end
 
